62aba955f3c98d602cf35adaa4121ef13bb4a672
Richard Camuccio <rcamuccio@gmail.com>
2019-06-20 11:43:19 -0500

    Added setup script

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name="simplemath",
+	version="1.0",
+	description="Simple Math Module",
+	author="Richard Camuccio",
+	author_email="rcamuccio@gmail.com",
+	py_modules=["simplemath", ]
+	)
