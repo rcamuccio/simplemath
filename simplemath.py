476003444da308d6241c28238a49629524046e4f
Richard Camuccio <rcamuccio@gmail.com>
2019-06-20 11:33:23 -0500

    Added docstrings

--- simplemath.py
+++ simplemath.py
@@ -1,24 +1,23 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
-# Richard Camuccio
-# 20 Jun 2019
-#
-# Last update: 20 Jun 2019
-#
-# simplemath.py
-#
+
+"""SimpleMath to do simple math
+
+Author: Richard Camuccio
+Copyright 2019
+"""
+
+MAX_ITER = 20
+"Docstring appears under global variables"
 
 def add(a, b):
-
+	"Add two numbers"
 	c = a + b
-
 	return c
 
 def main():
-
+	"Execute script"
 	print(add(2, 2))
 
 if __name__ == "__main__":
-
 	main()
